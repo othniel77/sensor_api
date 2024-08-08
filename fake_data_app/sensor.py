1292bd01e687f7b88acdb84134f1d0b387d4e3ca
root <root@LAPTOP-7EG6JJCD>
2024-08-08 19:59:25 +0200

    wip3

--- fake_data_app/sensor.py
+++ fake_data_app/sensor.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     queried_date = date(year, month, day)
 
     capteur = VisitSensor(1500, 150)
-    capteur2 = VisitSensor(1500, 150)
+    capteur2 = VisitSensor(2000, 150)
     print(capteur.get_visit_count(queried_date))
     print(capteur2.get_visit_count(queried_date))
     # python sensor.py > log.txt
